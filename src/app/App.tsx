3a4f3ca9212e0c5ab9857ace8e4b2afe88d98dce
Marcello Paiva <marcello.paiva@gmail.com>
2020-12-17 16:38:06 -0500

    set up app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {createClient, Provider} from 'urql';
 import Issues from './pages/issues';
 import Tokens from './pages/tokens';
 import AppContext from './providers/app-context';
@@ -11,18 +12,25 @@ const App = ({}) => {
     var {state} = React.useContext(AppContext);
     var renderPage;
 
-    switch (state.page) {
-        case '/issues':
-            renderPage = <Issues />;
-            break;
-        default:
-            if (state.loggedIn) {
-                renderPage = <Issues />;
-            } else {
-                renderPage = <Tokens />;
-            }
-            break;
-    }
+    const githubClient = createClient({
+        url: 'https://api.github.com/graphql',
+        fetchOptions: () => {
+            const token = 'token';
+            return {
+                headers: {authorization: token ? `Bearer ${token}` : ''},
+            };
+        },
+    });
+
+    const figmaClient = createClient({
+        url: 'http://localhost:3000/graphql',
+        fetchOptions: () => {
+            const token = 'tokem';
+            return {
+                headers: {authorization: token ? `Bearer ${token}` : ''},
+            };
+        },
+    });
 
     React.useEffect(() => {
         // This is how we read messages sent from the plugin controller
@@ -35,6 +43,32 @@ const App = ({}) => {
     // call launchControllerFunctions('message1') to launch the message1 command in src/plugin/controller.ts
     function launchControllerFunctions(messageType) {
         parent.postMessage({pluginMessage: {type: messageType}}, '*');
+    }
+
+    switch (state.page) {
+        case '/issues':
+            renderPage = <Issues />;
+            break;
+        case '/tokens':
+            renderPage = <Tokens />;
+            break;
+        default:
+            if (state.loggedIn) {
+                renderPage = <Issues />;
+            } else {
+                renderPage = <Tokens />;
+            }
+            break;
+    }
+
+    if (state.loggedIn) {
+        <Provider value={githubClient}>
+            <Provider value={figmaClient}>
+                <div className="root">{renderPage}</div>
+            </Provider>
+        </Provider>;
+    } else {
+        <div className="root">{renderPage}</div>;
     }
 
     return <div className="root">{renderPage}</div>;
